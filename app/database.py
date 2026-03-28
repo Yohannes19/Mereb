cb6e5b80561faa62451783cf58f2cd0b62fda6ad
Yohannes19 <joeabrha@gmail.com>
2026-03-28 12:30:05 +0100

    fix vercel

--- app/database.py
+++ app/database.py
@@ -1,24 +1,13 @@
 import os
 from collections.abc import Generator
-
 from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
+from sqlalchemy.orm import DeclarativeBase, sessionmaker, Session
 
-TURSO_DATABASE_URL = os.getenv("TURSO_DATABASE_URL")
-TURSO_AUTH_TOKEN = os.getenv("TURSO_AUTH_TOKEN")
-print("DB URL:", TURSO_DATABASE_URL)
-print("TOKEN:", TURSO_AUTH_TOKEN)
+DATABASE_URL = os.getenv("POSTGRES_URL")
 
-if TURSO_DATABASE_URL and TURSO_AUTH_TOKEN:
-    DATABASE_URL = f"{TURSO_DATABASE_URL}?authToken={TURSO_AUTH_TOKEN}"
-else:
-    DATABASE_URL = "sqlite:///./mvp.db"
+engine = create_engine(DATABASE_URL, connect_args={})  # Postgres does NOT need `check_same_thread`
 
-connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
-
-engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-
 
 class Base(DeclarativeBase):
     pass
