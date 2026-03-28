6cf0749e3f997e2c2fc2bcd25646ab367e07bfd1
Yohannes19 <joeabrha@gmail.com>
2026-03-28 18:08:28 +0100

    fix postgres dialect

--- app/database.py
+++ app/database.py
@@ -2,8 +2,17 @@ import os
 from collections.abc import Generator
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker, Session
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATABASE_URL = os.getenv("POSTGRES_URL")
+
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL not set")
 
 engine = create_engine(DATABASE_URL, connect_args={})  # Postgres does NOT need `check_same_thread`
 
